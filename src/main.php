7ff2305e47547f8d2abc72b35768aca6da8ee7e5
KatsuyaEndoh <k.endo@s-arcana.co.jp>
2019-02-18 20:07:16 +0900

    :bento: use phpsdl

--- src/main.php
+++ src/main.php
@@ -4,37 +4,38 @@ declare(strict_types=1);
 require_once('vendor/autoload.php');
 use Enkatsu\PhpOsc\Server;
 
-SDL_Init(SDL_INIT_EVERYTHING);
-$window = SDL_CreateWindow(
-  'Fixed pipeline example',
-  SDL_WINDOWPOS_CENTERED,
-  SDL_WINDOWPOS_CENTERED,
-  640, 480,
-  SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
-);
-SDL_GL_CreateContext($window);
-
-$server = new Server('localhost', 8338);
-function drawFace($raw)
+function draw($renderer, $server)
 {
-  $points = $raw->values->chunk(2);
-  // TODO: https://github.com/Ponup/phpopengl/blob/master/examples/rectangle.php
-}
-
-SDL_GL_SwapWindow($window);
-
-$event = new SDL_Event;
-while(true) {
-  // *** draw ***
-  glClearColor(0, 0, .2, 1);
-  glClear(GL_COLOR_BUFFER_BIT);
+  SDL_SetRenderDrawColor($renderer, 0, 0, 0, 255);
+  SDL_RenderClear($renderer);
   $bundle = $server->recieve();
   $raw = $bundle->getElement('/raw');
-  if(!is_null($raw)) drawFace($raw);
-  // *** event ***
-	SDL_PollEvent($event);
-	if($event->type == SDL_KEYDOWN) break;
-	SDL_Delay(50);
+  if(!is_null($raw)) drawFace($renderer, $raw);
+  SDL_RenderPresent($renderer);
 }
 
+function drawFace($renderer, $raw)
+{
+  SDL_SetRenderDrawColor($renderer, 255, 255, 255, 255);
+  $points = $raw->values->chunk(2)->values();
+  $points->each(function($point, $i) use ($renderer)
+  {
+    SDL_RenderDrawPoint($renderer, (int)$point->shift(), (int)$point->shift());
+  });
+}
+
+$server = new Server('localhost', 8338);
+$window = SDL_CreateWindow('Primitive drawing example', SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+$renderer = SDL_CreateRenderer($window, 0, SDL_RENDERER_SOFTWARE);
+$event = new SDL_Event;
+
+while(true)
+{
+  draw($renderer, $server);
+	SDL_PollEvent($event);
+	if($event->type == SDL_KEYDOWN) break;
+}
+
+SDL_DestroyRenderer($renderer);
 SDL_DestroyWindow($window);
+SDL_Quit();
